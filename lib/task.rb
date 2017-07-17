91fb13157fdc38c7afb2ae355468186524b2d6cc
Chris Nyaga <nyagachris75@gmail.com>
2017-07-18 00:08:48 +0300

    Before_save is a callback that has been used to run the private downcase_description method before any task is saved.

--- lib/task.rb
+++ lib/task.rb
@@ -1,7 +1,15 @@
 class Task < ActiveRecord::Base
   belongs_to(:list)
-  scope(:not_done, -> do
-      where({:done => false})
-    end)
-  validates(:description, presence:true,length:{maximum:50})
+  validates(:description, presence: true, length: { maximum: 50 })
+  before_save(:downcase_description)
+
+  scope(:not_done, lambda do
+    where(done: false)
+  end)
+
+  private
+
+  define_method(:downcase_description) do
+    self.description = description.downcase
+  end
 end

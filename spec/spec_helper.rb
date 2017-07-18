7b151073b96d1d79447fa102e393ccc8b5fd1b3c
Chris Nyaga <nyagachris75@gmail.com>
2017-07-18 10:27:29 +0300

    Spec helper file updated to use bundler to load gems and set it to test environment which will load also the test gems. Configuration for unit testing also added to file and rspec task to empty database refractored.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,17 @@
 ENV['RACK_ENV'] = 'test'
-require("rspec")
-  require("pg")
-  require("sinatra/activerecord")
-  require("task")
-  require("list")
 
-  RSpec.configure do |config|
-    config.after(:each) do
-      Task.all().each() do |task|
-        task.destroy()
-      end
-      List.all().each()do |list|
-        list.destroy()
-      end
-    end
+require('bundler/setup')
+Bundler.require(:default, :test)
+set(:root, Dir.pwd)
+
+require('capybara/rspec')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+require('./app')
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Task.all.each(&:destroy)
+    List.all.each(&:destroy)
   end
+end
